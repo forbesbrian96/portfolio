d050cc89de538719a8cd457a4aec6866705fdd94
Brian Forbes <brian@Brians-MacBook-Pro.local>
2023-05-03 22:00:09 -0400

    entered HTML, styling tomorrow

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <>
+      <div className="overlay">
+        <h1>Brian Forbes</h1>
+        <h1>Software Engineer</h1>
+
+        <h2>ABOUT ME</h2>
+
+        <p>
+          I’m a driven software engineer with expertise in programming
+          languages, frameworks, and databases, including JavaScript, Python,
+          React, Django, Java, PostgreSQL, and MongoDB. My collaborative nature
+          and strong problem-solving skills enable me to work well in a team
+          environment and approach challenges with a strategic mindset.
+        </p>
+
+        <p>
+          As a self-motivated and adaptable individual, I thrive in dynamic work
+          environments and welcome opportunities to expand my knowledge and
+          skill set. With a positive attitude and a commitment to excellence, I
+          am eager to contribute to innovative software development projects
+          that make a real impact.
+        </p>
+
+        <h2>PROJECTS</h2>
+
+        <h2>
+          <a href="https://docs.google.com/document/d/1FJlrP-NRLSSrXJuH9fXd9pMgiTZnUgFjlfMrylIi3-k/edit#heading=h.gjdgxs">
+            RESUME
+          </a>
+        </h2>
+
+        <h2>GET IN TOUCH</h2>
+
+        <p>
+          Please don’t hesitate to reach out if you have any questions, or
+          opportunities. I listen to all voicemails, and respond to all texts.
+          Any of the below listed contact methods will see a swift response.
+        </p>
+
+        <br />
+
+        <h4>Call or Text</h4>
+        <p>(843)-666-2070</p>
+
+        <h4>Email</h4>
+        <p>forbesbrian96@gmail.com</p>
+
+        <a href="https://github.com/forbesbrian96">GitHub</a>
+
+        <br />
+
+        <a href="https://www.linkedin.com/in/brian-forbes-swe/">LinkedIn</a>
+      </div>
+    </>
+  );
+};
+
+export default App;
